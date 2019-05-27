31b79c0db388d7eb16bcb2f67241f361314da052
Denis <denoms@bk.ru>
2019-05-27 21:17:46 +0600

    Lesson 2

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import img from '../error.png';
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <img src={img} alr="error"></img>
+    </>
+  )
+}
+
+export default ErrorMessage;
